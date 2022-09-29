8b3f00ede63f319faa02b9c48ad1e8ea072c8c02
Mannyg448 <manny.g448@hotmail.com>
2022-09-28 19:39:19 -0500

    added .env file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
 
   const { isAuthenticated, logout, user } = useAuth()
 
-  const brand = ref('🏢 Fake Company Directory')
+  const brand = ref(import.meta.env.VITE_APP_EMM)
 </script>
 
 <template>
